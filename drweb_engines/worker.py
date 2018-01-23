9c050ed8c534ce2e68c86894eb691901f7b071b8
uhbif19 <uhbif19@gmail.com>
2018-01-23 18:26:17 +0300

    fix:Баг в воркере.

--- drweb_engines/worker.py
+++ drweb_engines/worker.py
@@ -8,6 +8,6 @@ except:
 if __name__ == "__main__":
     if sys.platform.startswith("win"):
         # Под Windows не работает стандартны prefork-режим 
-        tasks.queue.worker_main(["-P", "solo"]+sys.argv)
+        tasks.queue.worker_main(sys.argv+["--pool", "solo"])
     else:
         tasks.queue.worker_main()
